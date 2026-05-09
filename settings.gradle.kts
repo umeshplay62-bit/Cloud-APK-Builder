8e93cf3f7db6f36ce6f90f0df673702a521a9d1f
umeshplay62-bit <umeshplay62@gmail.com>
2026-05-09 09:23:41 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,2 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = ("kotlin-android-template")
-
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-include(
-    "app",
-    "library-android",
-    "library-compose",
-    "library-kotlin"
-)
+rootProject.name = "Cloud-APK-Builder"
+include(":app")
