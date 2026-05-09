5654e67d3e54d16b8782270576b597a3c7ff772d
umeshplay62-bit <umeshplay62@gmail.com>
2026-05-09 09:31:39 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.cloud.apkbuilder"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.cloud.apkbuilder"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-}
-
-// यह बाहर वाली build.gradle.kts फाइल है
-plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
