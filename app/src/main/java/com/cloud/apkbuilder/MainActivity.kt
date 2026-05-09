0b2243a10c8f21ab34ba9965d8e89f1d26ffb915
umeshplay62-bit <umeshplay62@gmail.com>
2026-05-09 07:52:55 +0530

    Update MainActivity.kt

--- app/src/main/java/com/cloud/apkbuilder/MainActivity.kt
+++ app/src/main/java/com/cloud/apkbuilder/MainActivity.kt
@@ -2,9 +2,14 @@ package com.cloud.apkbuilder
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/index.html")
+        setContentView(webView)
     }
 }
