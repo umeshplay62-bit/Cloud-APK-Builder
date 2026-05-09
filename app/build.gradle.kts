712a5e12c40274f42fbbf3500add150e4c421903
umeshplay62-bit <umeshplay62@gmail.com>
2026-05-09 09:45:33 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.cloud.apkbuilder"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.cloud.apkbuilder"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
