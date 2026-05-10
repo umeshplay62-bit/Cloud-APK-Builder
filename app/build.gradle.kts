e7c3cd199cbf3e133a290420e07751fb52c17f92
umeshplay62-bit <umeshplay62@gmail.com>
2026-05-10 13:00:46 +0530

    Update build.gradle.kts
    लेब्रेडी

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,3 +30,9 @@ android {
         }
     }
 }
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+}
